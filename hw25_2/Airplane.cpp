84998bfdf90aa531a97dad3f757258d354f25f4c
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-08 05:12:15 +0100

    added the defenition of Airplane class

--- hw25_2/Airplane.cpp
+++ hw25_2/Airplane.cpp
@@ -1,1 +1,57 @@
 #include "Airplane.h"
+
+Airplane::Airplane(int flightNumber, string destination, int maxPassengers) {
+    this->flightNumber = flightNumber;
+    this->destination = destination;
+    this->currentPassengers = 0;
+    this->maxPassengers = maxPassengers;
+}
+
+int Airplane::getFlightNumber() const {
+    return flightNumber;
+}
+
+void Airplane::setFlightNumber(int flightNumber) {
+    this->flightNumber = flightNumber;
+}
+
+string Airplane::getDestination() const {
+    return destination;
+}
+
+void Airplane::setDestination(string destination) {
+    this->destination = destination;
+}
+
+int Airplane::getCurrentPassengers() const {
+    return currentPassengers;
+}
+
+void Airplane::addPassenger() {
+    if (currentPassengers < maxPassengers) {
+        currentPassengers++;
+    }
+    else {
+        cout << "The plane is full" << endl;
+    }
+}
+
+void Airplane::removePassenger() {
+    if (currentPassengers > 0) {
+        currentPassengers--;
+    }
+    else {
+        cout << "There isn't any passengers in the plane" << endl;
+    }
+}
+
+int Airplane::getMaxPassengers() const {
+    return maxPassengers;
+}
+
+void Airplane::printInfo() const {
+    cout << "Flight number: " << flightNumber << endl;
+    cout << "Destination: " << destination << endl;
+    cout << "Passengers count: " << currentPassengers << endl;
+    cout << "Maximum passengers in the plane: " << maxPassengers << endl;
+}
